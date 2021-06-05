0e84c73812836996eee75541446ab390abcd529b
christian89-alt <61234292+christian89-alt@users.noreply.github.com>
2021-06-05 21:41:48 +0200

    formulaire de contact via Firebase

---
+++ personnel/main.js
@@ -0,0 +1,60 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var firebaseConfig = {
+  apiKey: "AIzaSyDmogF4z0Yl-ys66PRaOb-068cloeCuLVA",
+  authDomain: "contact-form-7d1fb.firebaseapp.com",
+  projectId: "contact-form-7d1fb",
+  storageBucket: "contact-form-7d1fb.appspot.com",
+  messagingSenderId: "583257690848",
+  appId: "1:583257690848:web:d05167e844d349485f7685",
+  measurementId: "G-KLDYEW0JH0"
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
